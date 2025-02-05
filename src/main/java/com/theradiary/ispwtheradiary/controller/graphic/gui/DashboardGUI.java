5c51476c39ebac56c77f838799289bd7b32f2f8e
tostfia <tostisofia09@libero.it>
2025-02-05 15:48:08 +0100

    Tolto un import non usato

--- src/main/java/com/theradiary/ispwtheradiary/controller/graphic/gui/DashboardGUI.java
+++ src/main/java/com/theradiary/ispwtheradiary/controller/graphic/gui/DashboardGUI.java
@@ -1,6 +1,6 @@
 package com.theradiary.ispwtheradiary.controller.graphic.gui;
 
-import com.theradiary.ispwtheradiary.controller.graphic.gui.CommonGUI;
+
 import com.theradiary.ispwtheradiary.engineering.others.FXMLPathConfig;
 import com.theradiary.ispwtheradiary.engineering.others.Session;
 
